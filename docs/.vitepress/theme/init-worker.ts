4a48ee0d72498c92bfb7d1072ec19039f87f5ff2
susanforme <rzc307853639@gmail.com>
2025-07-23 16:45:52 +0800

    fix: 修复init-worker.ts中MonacoEnvironment初始化问题

--- docs/.vitepress/theme/init-worker.ts
+++ docs/.vitepress/theme/init-worker.ts
@@ -1,10 +1,11 @@
 /*
  * @Author: susanforme
  * @Date: 2025-07-23 16:43:15
- * @LastEditTime: 2025-07-23 16:43:20
+ * @LastEditTime: 2025-07-23 16:45:38
  * @FilePath: /blog/docs/.vitepress/theme/init-worker.ts
  * @Description:
  */
+
 //@ts-ignore
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 //@ts-ignore
@@ -16,20 +17,22 @@ import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
 //@ts-ignore
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
-self.MonacoEnvironment = {
-  getWorker(_, label) {
-    if (label === 'json') {
-      return new jsonWorker();
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker();
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker();
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker();
-    }
-    return new editorWorker();
-  },
-};
+if (typeof self !== 'undefined') {
+  self.MonacoEnvironment = {
+    getWorker(_, label) {
+      if (label === 'json') {
+        return new jsonWorker();
+      }
+      if (label === 'css' || label === 'scss' || label === 'less') {
+        return new cssWorker();
+      }
+      if (label === 'html' || label === 'handlebars' || label === 'razor') {
+        return new htmlWorker();
+      }
+      if (label === 'typescript' || label === 'javascript') {
+        return new tsWorker();
+      }
+      return new editorWorker();
+    },
+  };
+}
